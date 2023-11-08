1e36d0f1792cb16feef404926483e38f5ca3dd5c
Johan Van de Wauw <johanvdw@fosdem.org>
2023-11-08 01:24:39 +0100

    Actually send mail

--- devroom-settings/devroom_settings/management/commands/bulk_team_import.py
+++ devroom-settings/devroom_settings/management/commands/bulk_team_import.py
@@ -16,4 +16,6 @@ class Command(BaseCommand):
         mail = kwargs["mail"]
         team = Team.objects.get(pk=team_id)
         print(f"inviting {mail} to team {team.name}")
-        TeamInvite(email=mail, team=team).save()
+        invite=TeamInvite(email=mail, team=team)
+        invite.send()
+        invite.save()
