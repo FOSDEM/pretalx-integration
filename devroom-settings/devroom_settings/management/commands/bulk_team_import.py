b98d0a7b2ea18c4431522b4b44783b742f5c4fca
Johan Van de Wauw <johanvdw@fosdem.org>
2023-11-08 01:14:56 +0100

    Add utility script to import team members

---
+++ devroom-settings/devroom_settings/management/commands/bulk_team_import.py
@@ -0,0 +1,19 @@
+# import accepted devrooms from another CfP as tracks
+
+from django.core.management.base import BaseCommand
+from pretalx.event.models import TeamInvite, Team
+
+
+class Command(BaseCommand):
+    help = "Bulk import team members"
+
+    def add_arguments(self, parser):
+        parser.add_argument("team_id", help="team id")
+        parser.add_argument("mail", help="mail of person to be invited")
+
+    def handle(self, *args, **kwargs):
+        team_id = kwargs["team_id"]
+        mail = kwargs["mail"]
+        team = Team.objects.get(pk=team_id)
+        print(f"inviting {mail} to team {team.name}")
+        TeamInvite(email=mail, team=team).save()
