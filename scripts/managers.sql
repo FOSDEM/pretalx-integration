3a01158d15a67b06140ddde64bdd8edbe442fdb5
Johan Van de Wauw <johanvdw@fosdem.org>
2023-12-01 01:28:13 +0100

    Add sql script to update virtual mail

---
+++ scripts/managers.sql
@@ -0,0 +1,14 @@
+-- export devrooms for postfix virtual postmap file
+with managers as
+((select ts.mail, email FROM submission_track t
+join devroom_settings_tracksettings ts on t.id=ts.track_id
+join event_teaminvite et on et.team_id=ts.manager_team_id
+order by mail, email)
+union all
+(select ts.mail,  email FROM submission_track t
+join devroom_settings_tracksettings ts on t.id=ts.track_id
+join event_team_members em on em.team_id=ts.manager_team_id
+join person_user u on em.user_id=u.id
+order by mail, email))
+select mail||E'\t'||string_agg(email,',' order by email)||',devrooms@fosdem.org' from managers group by mail order by mail; 
+
