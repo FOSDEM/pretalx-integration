43b04776156ae07b4a0e909cbb0e1d791f657a53
Johan Van de Wauw <johanvdw@fosdem.org>
2023-12-11 20:58:31 +0100

    Export wip_schedule for the first scheduling weeks

--- devroom-settings/devroom_settings/management/commands/export_resources.py
+++ devroom-settings/devroom_settings/management/commands/export_resources.py
@@ -22,8 +22,12 @@ class Command(BaseCommand):
         dest_dir = Path(kwargs["destination_dir"])
 
         with scope(event=event):
+            if event.wip_schedule:
+                schedule = event.wip_schedule
+            else:
+                schedule = event.current_schedule
             nanoc_exporter = NanocExporter(
-                event=event, schedule=event.current_schedule, dest_dir=dest_dir
+                event=event, schedule=schedule, dest_dir=dest_dir
             )
             _, _, yaml_content = nanoc_exporter.render()
 
