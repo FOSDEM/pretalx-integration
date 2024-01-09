cddcce9a8f53e02b6efa4d7e1cf17b359ec4e103
Johan Van de Wauw <johanvdw@fosdem.org>
2024-01-09 21:16:14 +0100

    Auto release on website build

--- devroom-settings/devroom_settings/management/commands/export_resources.py
+++ devroom-settings/devroom_settings/management/commands/export_resources.py
@@ -1,6 +1,8 @@
+from datetime import datetime
 from pathlib import Path
 from shutil import copy2
 
+import pytz
 from django.core.management.base import BaseCommand
 from django_scopes import scope
 from PIL import Image
@@ -20,6 +22,16 @@ class Command(BaseCommand):
         event_slug = kwargs["event"]
         event = Event.objects.get(slug=event_slug)
         dest_dir = Path(kwargs["destination_dir"])
+
+        # create a release
+        if event.wip_schedule.changes["count"] > 0:
+            # Set the timezone to Europe/Brussels
+            brussels_timezone = pytz.timezone("Europe/Brussels")
+            # Get the current time in Brussels timezone
+            current_time = datetime.now(brussels_timezone)
+            # Format the current time to ISO 8601 with minute precision
+            current_time = current_time.strftime("%Y-%m-%d %H:%M")
+            event.wip_schedule.freeze(name=current_time, notify_speakers=False)
 
         with scope(event=event):
             schedule = event.current_schedule
