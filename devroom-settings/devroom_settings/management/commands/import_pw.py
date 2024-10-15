4f4445fea0d5b4112a12ef6f0146bd63e6e95d68
Johan Van de Wauw <johanvdw@fosdem.org>
2024-10-15 22:21:25 +0200

    Apply code formatting on import_pw

--- devroom-settings/devroom_settings/management/commands/import_pw.py
+++ devroom-settings/devroom_settings/management/commands/import_pw.py
@@ -1,25 +1,28 @@
 import csv
+
 from django.core.management.base import BaseCommand
 from django_scopes import scope
-from pretalx.schedule.models import Room
 from pretalx.event.models import Event
+from pretalx.schedule.models import Room
+
 from devroom_settings.models import RoomSettings
 
+
 class Command(BaseCommand):
-    help = 'Import room names and passwords from a CSV file into RoomSettings'
+    help = "Import room names and passwords from a CSV file into RoomSettings"
 
     def add_arguments(self, parser):
-        parser.add_argument('csv_file', type=str, help='Path to the CSV file')
+        parser.add_argument("csv_file", type=str, help="Path to the CSV file")
 
     def handle(self, *args, **options):
-        csv_file_path = options['csv_file']
+        csv_file_path = options["csv_file"]
 
-        with open(csv_file_path, 'r') as file:
+        with open(csv_file_path, "r") as file:
             reader = csv.DictReader(file)
 
             for row in reader:
-                room_name = row['room_name']
-                password = row['password']
+                room_name = row["room_name"]
+                password = row["password"]
 
                 event = Event.objects.get(pk=4)
                 with scope(event=event):
@@ -29,4 +32,8 @@ class Command(BaseCommand):
                 room_settings.control_password = password
                 room_settings.save()
 
-                self.stdout.write(self.style.SUCCESS(f'Successfully imported password for room: {room_name}'))
+                self.stdout.write(
+                    self.style.SUCCESS(
+                        f"Successfully imported password for room: {room_name}"
+                    )
+                )
