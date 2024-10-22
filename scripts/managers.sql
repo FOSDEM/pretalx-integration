e1a06ba5e176e78e003b7bf39cc235aa2fe73c8a
Johan Van de Wauw <johanvdw@fosdem.org>
2024-10-22 10:13:26 +0200

    Update managers.sql fo 2025

--- scripts/managers.sql
+++ scripts/managers.sql
@@ -4,13 +4,14 @@ with managers as
 join devroom_settings_tracksettings ts on t.id=ts.track_id
 join event_teaminvite et on et.team_id=ts.manager_team_id
 where ts.track_type='D'
+  and t.event_id=6
 order by mail, email)
 union all
 (select ts.mail,  email FROM submission_track t
 join devroom_settings_tracksettings ts on t.id=ts.track_id
 join event_team_members em on em.team_id=ts.manager_team_id
 join person_user u on em.user_id=u.id
-where ts.track_type='D'
+where ts.track_type='D' and t.event_id=6
 order by mail, email))
 select mail||E'\t'||string_agg(email,',' order by email)||',devrooms@fosdem.org' from managers group by mail order by mail; 
 
