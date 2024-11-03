b1c5a514a4200469f7bfc676e4ba45149a564148
Johan Van de Wauw <johanvdw@fosdem.org>
2024-11-03 18:30:49 +0100

    Specify event_id when adding devroom managers

--- scripts/managers-list.sql
+++ scripts/managers-list.sql
@@ -2,13 +2,13 @@ with managers as
 ((select ts.mail, email FROM submission_track t
 join devroom_settings_tracksettings ts on t.id=ts.track_id
 join event_teaminvite et on et.team_id=ts.manager_team_id
-where ts.track_type='D')
+where ts.track_type='D' and t.event_id=6)
 union all
 (select ts.mail,  email FROM submission_track t
 join devroom_settings_tracksettings ts on t.id=ts.track_id
 join event_team_members em on em.team_id=ts.manager_team_id
 join person_user u on em.user_id=u.id
-where ts.track_type='D'
+where ts.track_type='D' and t.event_id=6
 ))
 select distinct email from managers order by email;
 
