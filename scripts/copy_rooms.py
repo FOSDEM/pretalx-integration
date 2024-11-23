c10a27ef0f215edd5cd6dc229005f52349e43292
Johan Van de Wauw <johanvdw@fosdem.org>
2024-11-23 19:46:15 +0100

    Fix copy_rooms

--- scripts/copy_rooms.py
+++ scripts/copy_rooms.py
@@ -1,9 +1,11 @@
 # copies all rooms from one event to another
+from copy import copy
+
 from django_scopes import scope, scopes_disabled
 from pretalx.event.models import Event
 
-source_slug = "fosdem_2023"
-dest_slug = "fosdem_2024"
+source_slug = "fosdem-2024"
+dest_slug = "fosdem-2025"
 
 source = Event.objects.get(slug=source_slug)
 dest = Event.objects.get(slug=dest_slug)
@@ -16,5 +18,6 @@ with scope(event=dest):
         if dest.rooms.filter(name=room.name).exists():
             continue
         new_room = copy(room)
+        new_room.pk = None
         new_room.event = dest
         new_room.save()
