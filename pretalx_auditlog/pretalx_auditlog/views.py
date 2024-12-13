dc166eda581b17992bb36a0f2e404c1623becfc9
Johan Van de Wauw <johanvdw@fosdem.org>
2024-12-13 01:00:27 +0100

    Link changelog to modellog

--- pretalx_auditlog/pretalx_auditlog/views.py
+++ pretalx_auditlog/pretalx_auditlog/views.py
@@ -1,6 +1,9 @@
 import datetime
+import functools
 import inspect
 
+from django.apps import apps
+from django.db.models.fields.related import ForeignKey
 from django.utils.dateparse import parse_datetime
 from django.views.generic.base import TemplateView
 from pghistory.models import Events
@@ -15,6 +18,44 @@ models = list(inspect.getmembers(models, inspect.isclass))
 # returns a the list of (str, class)
 # we only want the names of the proxyevent classes
 model_names = [x[0] for x in models if "ProxyEvent" in x[0]]
+
+
+def set_extra_info(event):
+    """Get extra info for a model
+
+    Get useful data for an id, eg name or title
+    for a friendlier display"""
+    if "title" in event.pgh_data:
+        event.extra = event.pgh_data["title"]
+    elif "name" in event.pgh_data:
+        event.extra = event.pgh_data["name"]
+
+
+@functools.lru_cache(maxsize=1, typed=False)
+def get_foreign_key_mapping():
+    """
+    Create a dictionary mapping foreign key database column names to their
+    related models and fields.
+
+    Returns:
+        dict: A dictionary where the keys are foreign key database column names
+              and the values are tuples (<related_model_class>, <model_field>).
+    """
+    foreign_key_mapping = {}
+
+    # Ensure that the app registry is fully loaded
+    apps.check_apps_ready()
+
+    # Iterate over all installed models
+    for model in apps.get_models():
+        for field in model._meta.get_fields():
+            if isinstance(field, ForeignKey):
+                # Get the database column name
+                _, db_column_name = field.get_attname_column()
+                # Map the database column name to the related model and field
+                foreign_key_mapping[db_column_name] = field.related_model.__name__
+
+    return foreign_key_mapping
 
 
 class Changelog(PermissionRequired, TemplateView):
@@ -50,10 +91,9 @@ class Changelog(PermissionRequired, TemplateView):
                 .removesuffix("ProxyEvent")
                 .removeprefix("pretalx_auditlog.")
             )
-            if ev.short_model == "Submission":
-                ev.extra = ev.pgh_data["title"]
-            elif ev.short_model == "Track":
-                ev.extra = ev.pgh_data["name"]
+
+            set_extra_info(ev)
+
             if ev.pgh_context is not None:
                 try:
                     user = User.objects.get(pk=ev.pgh_context["user"])
@@ -64,7 +104,7 @@ class Changelog(PermissionRequired, TemplateView):
             initial={"n": 50, "max_date": max_date, "model": model},
             model_names=model_names,
         )
-        return {"events": events, "form": form}
+        return {"events": events, "form": form, "fk_map": get_foreign_key_mapping()}
 
 
 class Modellog(PermissionRequired, TemplateView):
@@ -76,6 +116,8 @@ class Modellog(PermissionRequired, TemplateView):
     def get_context_data(self, **kwargs):
         id = int(self.request.GET["id"])
         model = self.request.GET["model"]
+        if not model.endswith("ProxyEvent"):
+            model = model + "ProxyEvent"
         events = Events.objects.filter(
             pgh_model=f"pretalx_auditlog.{model}", pgh_obj_id=id
         ).order_by("-pgh_created_at")
