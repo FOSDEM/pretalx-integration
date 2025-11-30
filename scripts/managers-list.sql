39dc87f3824e9bf0e58bf01bc330f5507bd3e5da
Johan Van de Wauw <johanvdw@fosdem.org>
2025-11-30 20:06:47 +0100

    Update year

--- scripts/managers-list.sql
+++ scripts/managers-list.sql
@@ -8,7 +8,7 @@ union all
 join devroom_settings_tracksettings ts on t.id=ts.track_id
 join event_team_members em on em.team_id=ts.manager_team_id
 join person_user u on em.user_id=u.id
-where ts.track_type='D' and t.event_id=6
+where ts.track_type='D' and t.event_id=14
 ))
 select distinct email from managers order by email;
 
